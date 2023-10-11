3ed42afcbd038bdd8cd8e98891ae360372f23024
Daniel Lozano <daniel.lozano-escarti@capgemini.com>
2023-10-11 13:22:45 +0200

    Added core lib module with translations

---
+++ projects/core/src/lib/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@NgModule({
+  declarations: [],
+  imports: [TranslateModule],
+  exports: [TranslateModule],
+})
+export class CoreModule {}
